98ac73a11b2e2d12ed48bf5b90d80969cf6039d7
zeroornull <xxpfantastic@yahoo.com>
2024-12-30 23:52:15 +0800

    chore: 项目整体修改

---
+++ pmhub-base/pmhub-base-log/src/main/java/io/github/zeroornull/pmhub/common/log/enums/BusinessStatus.java
@@ -0,0 +1,20 @@
+package io.github.zeroornull.pmhub.common.log.enums;
+
+/**
+ * 操作状态
+ * 
+ * @author ruoyi
+ *
+ */
+public enum BusinessStatus
+{
+    /**
+     * 成功
+     */
+    SUCCESS,
+
+    /**
+     * 失败
+     */
+    FAIL,
+}
---
+++ pmhub-gateway/src/main/java/io/github/zeroornull/pmhub/gateway/config/GatewayConfig.java
@@ -0,0 +1,23 @@
+package io.github.zeroornull.pmhub.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import io.github.zeroornull.pmhub.gateway.handler.SentinelFallbackHandler;
+
+/**
+ * 网关限流配置
+ * 
+ * @author ruoyi
+ */
+@Configuration
+public class GatewayConfig
+{
+    @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    public SentinelFallbackHandler sentinelGatewayExceptionHandler()
+    {
+        return new SentinelFallbackHandler();
+    }
+}
